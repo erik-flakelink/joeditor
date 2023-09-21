b1041471e98e3955c0c59909ba358a0b015c3c9f
Mr. RDEV <122167559+mr-rdev@users.noreply.github.com>
2023-09-21 17:12:00 -0400

    Update joeditor.py

--- joeditor.py
+++ joeditor.py
@@ -2,21 +2,21 @@ from tkinter import *
 #joeditor
 #By Erik "Joe" Pastori
 
+from tkinter import Tk, font
+
+root = Tk()
+options = root.call('font', 'names')
+root.destroy()
+
+
 root = Tk()
 root.geometry("250x400")
 joefont = ["Comic Sans MS", 20, "normal"]
-
-options = [
-    "Comic Sans MS",
-    "Times New Roman"
-]
+#Found the above in a github, seems useful!
 
 clicked = StringVar()
 
 clicked.set(joefont[0])
-
-drop = OptionMenu( root , clicked , *options )
-drop.pack()
 
 icon = PhotoImage(file="Joeditor.png") 
 root.iconphoto(False, icon)
@@ -33,14 +33,39 @@ text = Text(root, yscrollcommand=scroll.set, font=joetuple)
 
 scroll.config(command=text.yview)
 
-def changefont():
-    global joefont
-    global joetuple
-    global clicked
-    joefont[0] = clicked.get()
-    joetuple = (joefont[0], joefont[1], joefont[2])
-    
-changefontbutton = Button(root, text="Change Font", command=changefont)
+def apply_font(selected_font):
+    text.config(font=(selected_font, joefont[1]))
+
+def font():
+    #ChatGPT Assistance, I made the font adjustments to the menu, changing font based on name
+    root = Tk()
+    root.title("Font List")
+
+    font_list = root.call("font", "families")
+
+    frame = Frame(root)
+    frame.pack(fill=BOTH, expand=True)
+
+    canvas = Canvas(frame)
+    canvas.pack(side=LEFT, fill=Y)
+
+    scrollbar = Scrollbar(frame, command=canvas.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    inner_frame = Frame(canvas)
+    canvas.create_window((0, 0), window=inner_frame, anchor=NW)
+
+    for font_name in font_list:
+        button = Button(inner_frame, font=(font_name, 17), text=font_name, command=lambda name=font_name: apply_font(name))
+        button.pack(fill=X, padx=5, pady=2)
+
+    inner_frame.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+
+    root.mainloop()
+changefontbutton = Button(root, text="Font", command=font)
 changefontbutton.pack()
 
 text.pack()
