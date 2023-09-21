e62107daac27bfde08dd40bb4c87302b0fc6282b
Mr. RDEV <122167559+mr-rdev@users.noreply.github.com>
2023-09-21 13:54:49 -0400

    Update joeditor.py

--- joeditor.py
+++ joeditor.py
@@ -6,6 +6,18 @@ root = Tk()
 root.geometry("250x400")
 joefont = ["Comic Sans MS", 20, "normal"]
 
+options = [
+    "Comic Sans MS",
+    "Times New Roman"
+]
+
+clicked = StringVar()
+
+clicked.set(joefont[0])
+
+drop = OptionMenu( root , clicked , *options )
+drop.pack()
+
 icon = PhotoImage(file="Joeditor.png") 
 root.iconphoto(False, icon)
 root.title("Joeditor")
@@ -20,5 +32,16 @@ joetuple = (joefont[0], joefont[1], joefont[2])
 text = Text(root, yscrollcommand=scroll.set, font=joetuple)
 
 scroll.config(command=text.yview)
+
+def changefont():
+    global joefont
+    global clicked
+    joefont[0] = clicked.get()
+    joetuple = (joefont[0], joefont[1], joefont[2])
+    
+changefontbutton = Button(root, text="Change Font", command=changefont)
+changefontbutton.pack()
+
 text.pack()
+
 root.mainloop()
